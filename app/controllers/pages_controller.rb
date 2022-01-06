1a3b8345351fb0b13e0417ff3b543cc4df753bd3
Leon Boutrand <leonboutrand@gmail.com>
2022-01-06 20:29:41 +0100

    starting using nokogiri for gpx

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
+
+  def parseGPX
+    file = File.open('app/assets/gpx/sgmx.gpx')
+    doc = Nokogiri::XML(open(file))
+    trackpoints = doc.xpath('//xmlns:trkpt')
+    trackpoints.map do |trkpt|
+      [trkpt.xpath('@lat').to_s.to_f, trkpt.xpath('@lon').to_s.to_f, trkpt.xpath('ele').text.to_f]
+    end
+  end
+
   def main
     @markers = [{
       lat: 35,
